30e971a983b03d380b6eea14476073cb83c3095d
jacksonohallo <jacksonohallo@gmail.com>
2022-11-29 03:38:04 -0800

    feat: add social icons

--- src/components/nabar/Navbar.js
+++ src/components/nabar/Navbar.js
@@ -1,24 +1,25 @@
 import styled from "@emotion/styled";
-import { AppBar, Toolbar } from "@mui/material";
-import { Box } from "@mui/system";
+import { AppBar, Toolbar, Box } from "@mui/material";
 import React from "react";
+import { Facebook, Instagram, Twitter } from "@mui/icons-material";
 
-
-const StyledToolbar= styled(Toolbar)({
-display: "flex",
-justifyContent: "space-between"
-
-})
-
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
 
 const Navbar = () => {
   return (
     <AppBar>
       <StyledToolbar>
-        <Box>Navbar</Box>
+        <Box>
+          <Facebook />
+          <Twitter />
+          <Instagram />
+        </Box>
         <Box>Menu</Box>
         <Box>Social</Box>
-        </StyledToolbar>
+      </StyledToolbar>
     </AppBar>
   );
 };
