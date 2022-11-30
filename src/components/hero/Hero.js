a18604a17518c1bac80b2325d40f9622cc4f5c75
jacksonohallo <jacksonohallo@gmail.com>
2022-11-29 22:57:17 -0800

    feat:add images

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,8 +1,39 @@
+import { Box, Typography } from  "@mui/material";
 import React from 'react'
-
+// import HeroImage from "../../static/HeroImage.jpg"
+import HeroImage from "../../static/HeroImage.jpg";
 const Hero = () => {
   return (
-    <div>Hero</div>
+    
+    <Box>
+<Typography varient="h3" align="center" sx={{fontWeight:900}}>
+FASHION<b style={{color:"red"}}>BLOG</b>
+</Typography>
+<Typography varient="body2" align="center"sx={{fontWeight:100}}>
+We make you look better for you
+</Typography>
+    <Box sx={{
+    backgroundImage: `url(${HeroImage})`,
+    // backgroundImage: `url(${HeroImage})`,
+    BackgroundColor:"black",
+    BackgroundPosition:"center",
+    BackgroundRepeat:"no-repeat",
+    BackgroundSize:"cover",
+    height:600,
+    width:"100%"
+    
+
+    }}>
+
+
+    </Box>
+
+
+
+    </Box>
+
+
+
   )
 }
 
