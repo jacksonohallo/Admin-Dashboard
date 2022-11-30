4249a31e5734ea9f3b7783dfa05593dfb686be0c
jacksonohallo <jacksonohallo@gmail.com>
2022-11-29 21:51:21 -0800

    refactor: changed function to arrow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
 import Navbar from "./components/nabar/Navbar";
+import Hero from "./components/hero/Hero";
 
-function App() {
-  return <Navbar />;
-}
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+};
 
 export default App;
