16fb86c6c30e7d3bc1f7bb26cad400e062a29ee3
jacksonohallo <jacksonohallo@gmail.com>
2022-11-30 01:56:19 -0800

    feat: add images to categories section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/nabar/Navbar";
 import Hero from "./components/hero/Hero";
+import Categories from "./components/category/Categories"
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Categories/>
     </>
   );
 };
---
+++ src/components/category/Categories.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {Box,Stack,styled, Typography} from "@mui/material"
+import kidImage  from  '../../static/kids.jpg'
+import womenImage from '../../static/women.jpg'
+import menImage  from '../../static/men.jpg'
+
+
+const StyledBox=styled(Box)({
+height: 200,
+width: "100%",
+cursor: "pointer",
+backgroundSize: "cover",
+backgroundRepeat: "no-repeat",
+backgroundPosition: "center center"
+});
+
+const StyledTypography=styled(Typography)({
+margin: "25px 50px 25px 50px",
+background: "white",
+opacity: "0.8"
+
+})
+
+const Categories = () => {
+  return (
+<Box>
+<Stack direction={"row"} spacing={4}>
+   <StyledBox sx={{backgroundImage:`url(${kidImage})`}}>
+    <StyledTypography align="center" varient="h3">Kid</StyledTypography></StyledBox> 
+   <StyledBox sx={{backgroundImage:`url(${womenImage})` }}>
+    <StyledTypography align="center" varient="h3">Men</StyledTypography></StyledBox> 
+   <StyledBox sx ={{background:`url(${menImage})`}}>
+   <StyledTypography align="center" varient="h3">Women</StyledTypography></StyledBox> 
+ 
+
+
+</Stack>
+
+
+
+    </Box>
+  )
+}
+
+export default Categories
